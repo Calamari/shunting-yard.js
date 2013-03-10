a429ff5c8e6b43f6070d56d6322197e481013b7a
calamari <g.tavonius@gmail.com>
2013-03-10 18:18:01 +0100

    first version without paranthesis

--- spec/javascripts/shunting_yard_spec.js
+++ spec/javascripts/shunting_yard_spec.js
@@ -1,17 +1,21 @@
 describe("jaz.shuntingYard", function() {
   var testCase = function(key, val) {
-    it('"' + key + '" => ' + val, function() {
-      expect(jaz.shuntingYard(key)).toBe(val);
+    it('"' + key + '" => ' + val.join(' '), function() {
+      expect(jaz.shuntingYard(key)).toEqual(val);
     });
   };
 
   describe("resolves", function() {
     var positiveTest = {
       '10+3': ['10', '3', '+'],
-      '10-3': ['10', '3', '-'],
+      '10 - 3': ['10', '3', '-'],
       '10.5-3.25': ['10.5', '3.25', '-'],
       '42*0.1': ['42', '0.1', '*'],
-      '42/0.1': ['42', '0.1', '/']
+      '42/0.1': ['42', '0.1', '/'],
+      '-42': ['-42'],
+      '+23': ['+23'],
+      '12^2': ['12', '2', '^'],
+      '-2 * 4': ['-2', '4', '*']
     };
     for (var key in positiveTest) {
       testCase(key, positiveTest[key]);
--- src/shunting_yard.js
+++ src/shunting_yard.js
@@ -4,12 +4,54 @@
  * @author Georg Tavonius a.k.a. Calamari (http://github.com/Calamari)
  * @homepage http://github.com/Calamari/shunting-yard.js
  */
+ /**
+   TODO:
+     - change tokenizer from '' to whitespace for example
+     - change operators
+  */
 (function() {
   'use strict';
   var jaz = window.jaz || {};
 
   jaz.shuntingYard = function(string) {
-    return ''
+    var operators = ['+', '-', '*', '/', '^'],
+        output = [],
+        stack = [],
+        lastToken,
+        i, l, token;
+
+    function isOperator(token) {
+      return operators.indexOf(token) !== -1;
+    }
+
+    function popStackToOutput() {
+    }
+
+    for (i=0,l=string.length; i<l; ++i) {
+      token = string[i];
+      if (token === ' ') {
+        continue; // do nothing with spaces
+      }
+      if (i === 0) {
+        output.push(token);
+      } else if (isOperator(token)) {
+        stack.push(token);
+      } else {
+        if (!lastToken || isOperator(lastToken) && i!==1) {
+          output.push(token);
+        } else {
+          output[output.length-1] += token;
+        }
+      }
+      lastToken = token;
+    }
+
+    // append the rest of the stack to the output
+    for (i=0,l=stack.length;i<l;++i) {
+      output.push(stack[i]);
+    }
+
+    return output;
   };
 
   window.jaz = jaz;
