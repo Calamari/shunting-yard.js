200b6aa9455fe66bffab802b55e6a5f78a2fec34
calamari <g.tavonius@gmail.com>
2013-03-11 22:20:41 +0100

    test that shuntingYard accepts arrays

--- spec/javascripts/shunting_yard_spec.js
+++ spec/javascripts/shunting_yard_spec.js
@@ -31,6 +31,27 @@ describe("jaz.shuntingYard", function() {
     }
   });
 
+  describe("resolves this tokenized arrays", function() {
+    var positiveTest = {
+      '10 + 3': ['10', '3', '+'],
+      '10 - 3': ['10', '3', '-'],
+      '10.5 - 3.25': ['10.5', '3.25', '-'],
+      '42 * 0.1': ['42', '0.1', '*'],
+      '42 / 0.1': ['42', '0.1', '/'],
+      '-42': ['-42'],
+      '+23': ['+23'],
+      '12 ^ 2': ['12', '2', '^'],
+      '-2 * 4': ['-2', '4', '*'],
+      '( 10 + 3 )': ['10', '3', '+'],
+      '10 + 3 * 3': ['10', '3', '3', '*', '+'],
+      '( -10 + 3 )': ['-10', '3', '+'],
+      '3 + 4 * 2 / ( 1 - 5 ) ^ 2 ^ 3': ['3', '4', '2', '*', '1', '5', '-', '2', '3', '^', '^', '/', '+'],
+      '-1 * ( -4 + 2 )': ['-1', '-4', '2', '+', '*']
+    };
+    for (var key in positiveTest) {
+      testCase(key.split(' '), positiveTest[key]);
+    }
+  });
   it('returns null if to many paranthesis are closed', function() {
     expect(jaz.shuntingYard('1*(3+4))')).toEqual(null);
   });
